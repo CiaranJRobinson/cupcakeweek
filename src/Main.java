5415dc1f3b4251595457094f8847a8a4409828cc
CiaranJRobinson <ciaranjayrobinson@gmail.com>
2022-09-27 15:51:04 -0400

    part 3 done

--- src/Main.java
+++ src/Main.java
@@ -3,60 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        class Cupcake {
-            public double price;
-
-            public double getPrice() {
-                return price;
-            }
-
-            public void setPrice(double price) {
-                this.price = price;
-            }
-
-            public void type(){
-                System.out.println("A basic, generic cupcake, with vanilla frosting");
-            }
-
-        }
-
-        class RedVelvet extends Cupcake{
-            public void type(){
-                System.out.println("A red velvet based cupcake, with cream cheese frosting.");
-            }
-        }
-
-        class Chocolate extends Cupcake{
-            public void type(){
-                System.out.println("A chocolate based cupcake, with chocolate frosting.");
-            }
-        }
-
-        class Drink {
-            public double price;
-            public double getPrice() {
-                return price;
-            }
-            public void setPrice(double price) {
-                this.price = price;
-            }
-           public void type(){
-               System.out.println("A bottle of water");
-           }
-        }
-
-        class Soda extends Drink{
-            public void type(){
-                System.out.println("A bottle of soda");
-            }
-        }
-
-        class Milk extends Drink{
-            @Override
-            public void type() {
-                System.out.println("A bottle of milk");
-            }
-        }
 
         ArrayList<Cupcake> cupcakeMenu = new ArrayList<>();
         Cupcake cupcake = new Cupcake();
@@ -122,5 +68,62 @@ public class Main {
         drinkMenu.add(water);
         drinkMenu.add(soda);
         drinkMenu.add(milk);
+
+        new Order(cupcakeMenu, drinkMenu);
     }
 }
+
+        class Cupcake {
+            public double price;
+
+            public double getPrice() {
+                return price;
+            }
+
+            public void setPrice(double price) {
+                this.price = price;
+            }
+
+            public void type(){
+                System.out.println("A basic, generic cupcake, with vanilla frosting");
+            }
+
+        }
+
+        class RedVelvet extends Cupcake{
+            public void type(){
+                System.out.println("A red velvet based cupcake, with cream cheese frosting.");
+            }
+        }
+
+        class Chocolate extends Cupcake{
+            public void type(){
+                System.out.println("A chocolate based cupcake, with chocolate frosting.");
+            }
+        }
+
+        class Drink {
+            public double price;
+            public double getPrice() {
+                return price;
+            }
+            public void setPrice(double price) {
+                this.price = price;
+            }
+           public void type(){
+               System.out.println("A bottle of water");
+           }
+        }
+
+        class Soda extends Drink{
+            public void type(){
+                System.out.println("A bottle of soda");
+            }
+        }
+
+        class Milk extends Drink{
+            @Override
+            public void type() {
+                System.out.println("A bottle of milk");
+            }
+        }
